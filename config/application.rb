de645fd43b6b952db35b5cdd0e2169b725f392c7
alegalliard <ale.sz.design@gmail.com>
2017-10-22 22:00:24 -0200

    i18n + root

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,16 @@ module AluguelTemporada
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Brasilia'
+
+    config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
   end
 end
