5ac43584b9a00ae7315a047d2d5a64b831058c2f
alegalliard <ale.sz.design@gmail.com>
2017-10-29 23:52:10 -0200

    add new property

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,4 +2,22 @@ class PropertiesController < ApplicationController
   def new
     @property = Property.new
   end
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      flash[:notice] = 'Imóvel cadastrado com sucesso!'
+      redirect_to @property
+    end
+  end
+  def show
+    @property = Property.find(params[:id])
+  end
+  private
+  def property_params
+    params.require(:property).permit(:property_location, :area, :title, :description,
+                                   :daily_rate, :rooms, :minimum_rent_days,
+                                   :maximum_rent_days, :photo,
+                                   :maximum_occupancy, :usage_rules,
+                                   :property_type)
+  end
 end
